84f75fa4a7824e52eec937372f031a68c4302c21
SilvanDomino <silvah489@gmail.com>
2020-10-09 10:02:10 +0200

    Shuffle sttring function added

--- lib/myWidget.dart
+++ lib/myWidget.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget{
+class MyWidget extends StatefulWidget{
+  MyWidgetState createState() => MyWidgetState();
+}
+
+class MyWidgetState extends State<MyWidget>{
   Widget build(BuildContext context){
     return Container(
         height: 10,
-        width: 20,
+        width: 200,
         color: Colors.black,
         child: Text("F"),
       );
